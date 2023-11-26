1911bded1d1a119083e0445aa045a156e12f684a
Max von Klemperer <maxvonklemperer@gmail.com>
2023-11-26 15:27:31 +0100

    building up to allow insert and update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
 from database import SessionLocal
 from database import engine
 from models import CountryData
@@ -8,16 +9,6 @@ import models
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
 
 
 def get_db():
@@ -29,6 +20,46 @@ def get_db():
 
 
 db_dependency = Annotated[SessionLocal, Depends(get_db)]
+
+
+class CountryDataRequest(BaseModel):
+    id: int
+    country: str
+    year: int
+    iso_code: str
+    population: int
+    gdp: float
+    co2: float
+    co2_per_capita: float
+    co2_per_gdp: float
+    co2_per_unit_energy: float
+    coal_co2: float
+    coal_co2_per_capita: float
+    consumption_co2: float
+    consumption_co2_per_capita: float
+    consumption_co2_per_gdp: float
+    cumulative_co2: float
+    cumulative_coal_co2: float
+    energy_per_capita: float
+    energy_per_gdp: float
+    flaring_co2: float
+    flaring_co2_per_capita: float
+    gas_co2: float
+    gas_co2_per_capita: float
+    ghg_excluding_lucf_per_capita: float
+    ghg_per_capita: float
+    land_use_change_co2: float
+    land_use_change_co2_per_capita: float
+    methane: float
+    methane_per_capita: float
+    nitrous_oxide: float
+    nitrous_oxide_per_capita: float
+    primary_energy_consumption: float
+    share_of_temperature_change_from_ghg: float
+    total_ghg: float
+    total_ghg_excluding_lucf: float
+    trade_co2: float
+    trade_co2_share: float
 
 
 @app.get("/allData")
