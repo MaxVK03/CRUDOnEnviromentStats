f68a7e88a2e0f47cfdfadae67b9524e903e6bb55
Max von Klemperer <maxvonklemperer@gmail.com>
2023-11-26 13:50:02 +0100

    Added Getters for ISO and Country name with year filtering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
-from database import SessionLocal, engine
+from fastapi import FastAPI, Depends, HTTPException
+from database import SessionLocal
 from database import engine
 from models import CountryData
 import models
@@ -36,4 +36,66 @@ async def allData(db: db_dependency):
     return db.query(CountryData).all()
 
 
+@app.get("/country/CName/{countryid}/afterYear/{yearid}")
+async def country(countryid: str, yearid: int, db: db_dependency):
+    todo_model = db.query(CountryData).filter(CountryData.country == countryid).filter(CountryData.year > yearid).all()
+    if todo_model is None or len(todo_model) == 0:
+        return HTTPException(status_code=400, detail='Item not found')
+    return todo_model
 
+
+@app.get("/country/CName/{countryid}/beforeYear/{yearid}")
+async def country(countryid: str, yearid: int, db: db_dependency):
+    todo_model = db.query(CountryData).filter(CountryData.country == countryid).filter(CountryData.year < yearid).all()
+    if todo_model is None or len(todo_model) == 0:
+        return HTTPException(status_code=400, detail='Item not found')
+    return todo_model
+
+
+@app.get("/country/CName/{countryid}/year/{yearid}")
+async def country(countryid: str, yearid: int, db: db_dependency):
+    todo_model = db.query(CountryData).filter(CountryData.country == countryid).filter(CountryData.year == yearid).all()
+    if todo_model is None or len(todo_model) == 0:
+        return HTTPException(status_code=400, detail='Item not found')
+    return todo_model
+
+
+@app.get("/country/CName/{countryid}")
+async def country(countryid: str, db: db_dependency):
+    todo_model = db.query(CountryData).filter(CountryData.country == countryid).all()
+    if todo_model is None or len(todo_model) == 0:
+        return HTTPException(status_code=400, detail='Item not found')
+    return todo_model
+
+
+@app.get("/country/CIso/{countryid}/afterYear/{yearid}")
+async def country(countryid: str, yearid: int, db: db_dependency):
+    todo_model = db.query(CountryData).filter(CountryData.iso_code == countryid).filter(CountryData.year > yearid).all()
+    if todo_model is None or len(todo_model) == 0:
+        return HTTPException(status_code=400, detail='Item not found')
+    return todo_model
+
+
+@app.get("/country/CIso/{countryid}/beforeYear/{yearid}")
+async def country(countryid: str, yearid: int, db: db_dependency):
+    todo_model = db.query(CountryData).filter(CountryData.iso_code == countryid).filter(CountryData.year < yearid).all()
+    if todo_model is None or len(todo_model) == 0:
+        return HTTPException(status_code=400, detail='Item not found')
+    return todo_model
+
+
+@app.get("/country/CIso/{countryid}/year/{yearid}")
+async def country(countryid: str, yearid: int, db: db_dependency):
+    todo_model = db.query(CountryData).filter(CountryData.iso_code == countryid).filter(
+        CountryData.year == yearid).all()
+    if todo_model is None or len(todo_model) == 0:
+        return HTTPException(status_code=400, detail='Item not found')
+    return todo_model
+
+
+@app.get("/country/CIso/{countryid}")
+async def country(countryid: str, db: db_dependency):
+    todo_model = db.query(CountryData).filter(CountryData.iso_code == countryid).all()
+    if todo_model is None or len(todo_model) == 0:
+        return HTTPException(status_code=400, detail='Item not found')
+    return todo_model
