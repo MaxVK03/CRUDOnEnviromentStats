f21a65bbe93e688cf0cb624570661ca5d5d7b31f
Max von Klemperer <maxvonklemperer@gmail.com>
2023-12-13 15:22:06 +0100

    rewrote the get part of the api

--- routers/country_routes.py
+++ routers/country_routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, Query
+from http.client import HTTPException
+
+from fastapi import APIRouter, Depends
 from Database.database_utils import get_db
 from services import country_service
 from models import CountryDataRequest
@@ -6,338 +8,105 @@ from models import CountryDataRequest
 router = APIRouter()
 db_dependency = Depends(get_db)
 
-# TODO: Add errors for not found
-# TODO: Add returns for creation
-# TODO: Add returns for update
 
-
-@router.get("/allData/")
-async def get_all_data(db=db_dependency):
+@router.get("/country/data")
+async def get_country_data(countryName: str = None, countryIsocode: str = None,
+                           yearid: int = None, timeFrame: str = None, db=db_dependency):
     """
-    Get all data from the database
+    Retrieve country data based on various criteria.
+    Access: GET /country/data
+    - countryName: Optional[str] - The name of the country.
+    - countryIsocode: Optional[str] - The iso code of the country.
+    - yearid: Optional[int] - The year of the data.
+    :param timeFrame:
+    :param yearid:
+    :param countryIsocode:
+    :param countryName:
+    :param db: The database session.
+    :return: Country data based on the provided criteria.
 
-    name: '/allData'
-
-    Access: GET /allData
-        - No parameters
-
-    :param db: Takes in the database session, No user input required.
-
-    :return: Returns all data from the database.
     """
-    return country_service.get_all_data(db)
+    if countryName and yearid and timeFrame:
+        return country_service.get_country_data(db=db, countryName=countryName, iso=None,
+                                                yearid=yearid, timeFrame=timeFrame)
+    elif countryIsocode and yearid and timeFrame:
+        return country_service.get_country_data(db=db, countryName=None, iso=countryIsocode,
+                                                yearid=yearid, timeFrame=timeFrame)
+    else:
+        return 'Invalid parameters.'
 
-
-@router.get("/country/C_Name/{countryName}/year/{yearid}")
-async def get_country_by_name_and_year(countryName: str, yearid: int, db=db_dependency):
-    """
-    Retrieve country data by country name and year.
-
-    name: '/country/C_Name/{countryName}/year/{yearid}'
-
-    Access: GET /country/C_Name/{countryName}/year/{yearid}
-        - countryName: String - The name of the country.
-        - yearid: int - The year of the data.
-
-    :param countryName: The name of the country.
-
-    :param yearid: the year of the data.
-
-    :param db: Takes in the database session.
-
-    :return: Returns that country data for that year.
-    """
-    return country_service.get_country_data_by_name_and_year(db, countryName, yearid)
-
-
-# TODO: Add returns for creation
 @router.post("/country")
 async def create_country(countrydt: CountryDataRequest, db=db_dependency):
     """
     Create a new country to add to the database.
-
-    name: '/country'
-
     Access: POST /country
-        - countrydt: CountryData The country data to be added to the database.
-
-    :param countrydt: The country data to be added to the database.
-
+    :param countrydt: The country data to be added.
     :param db: The database session.
-
-    :return: Nothing
+    :return: Result of the creation operation.
     """
     return country_service.create_country_data(db, countrydt)
 
 
-@router.put("/country/{countryName}")
-async def update_country_by_name(countryName: str, countrydt: CountryDataRequest, db=db_dependency):
+@router.put("/country")
+async def update_country(countrydt: CountryDataRequest, db=db_dependency, countryName: str = None,
+                         countryIsocode: str = None):
     """
-    Update a country by name.
-
-    name: '/country/{countryName}'
-
-    Access: PUT /country/{countryName}
-        - countryName: String - The name of the country to be updated.
-
-    :param countryName: The name of the country to be updated.
-
-    :param countrydt: A country data request object containing the data to be updated Allows for
-    validation.
-
+    Update country data by name or iso code.
+    Access: PUT /country
+    :param countryName: Optional[str] - The name of the country.
+    :param countryIsocode: Optional[str] - The iso code of the country.
+    :param countrydt: A country data request object containing the data to be updated.
     :param db: The database session.
-
-    :return: Nothing
+    :return: Result of the update operation.
     """
-    return country_service.update_country_data_by_name(db, countryName, countrydt)
+    if countryName:
+        return country_service.update_country_data_by_name(db, countryName, countrydt)
+    elif countryIsocode:
+        return country_service.update_country_data_by_isocode(db, countryIsocode, countrydt)
 
 
-@router.delete("/country/{countryName}/{yearid}")
-async def delete_country_by_name_and_year(countryName: str, yearid: int, db=db_dependency):
+@router.delete("/country")
+async def delete_country(countryName: str = None, countryIsocode: str = None,
+                         yearid: int = None, timeFrame: str = None, db=db_dependency):
     """
-    Delete a country by name and year.
-
-    name: '/country/{countryName}/{yearid}'
-
-    Access: DELETE /country/{countryName}/{yearid}
-        - countryName: String - The name of the country to be deleted.
-        - yearid: int - The year of the data to be deleted.
-
-    :param countryName: The name of the country to be deleted.
-    :param yearid: The year of the data to be deleted.
+    Delete a country by name, iso code, and/or year.
+    Access: DELETE /country
+    :param timeFrame:
+    :param countryName: Optional[str] - The name of the country.
+    :param countryIsocode: Optional[str] - The iso code of the country.
+    :param yearid: Optional[int] - The year of the data.
     :param db: The database session.
-    return: Nothing
+    :return: Result of the deletion operation.
     """
-    return country_service.delete_country_data_by_name_and_year(db, countryName, yearid)
+    if countryName and yearid and timeFrame:
+        return country_service.delete_country_data_by_name_and_year_and_timeFrame(db, countryName, yearid, timeFrame)
+    elif countryIsocode and yearid and timeFrame:
+        return country_service.get(db, countryIsocode, yearid, timeFrame)
+    elif countryName and yearid:
+        return country_service.delete_country_data_by_name_and_year(db, countryName, yearid)
+    elif countryIsocode and yearid:
+        return country_service.delete_country_data_by_isocode_and_year(db, countryIsocode, yearid)
+    elif countryName:
+        return country_service.delete_country_data_by_name(db, countryName)
+    elif countryIsocode:
+        return country_service.delete_country_data_by_isocode(db, countryIsocode)
 
 
-@router.get("/country/c_isocode/{countryIsocode}")
-async def get_country_by_isocode(countryIsocode: str, db=db_dependency):
+@router.get("/country/emissions")
+async def get_country_emissions(countryName: str = None, countryIsocode: str = None,
+                                yearid: int = None, db=db_dependency):
     """
-    Retrieve country data by country iso code.
-
-    Name: '/country/c_isocode/{countryIsocode}'
-
-    Access: GET /country/c_isocode/{countryIsocode}
-        - countryIsocode: String - The iso code of the country.
-
-    :param countryIsocode: The iso code of the country.
+    Retrieve country emissions data based on various criteria.
+    Access: GET /country/emissions
+    :param countryName: Optional[str] - The name of the country.
+    :param countryIsocode: Optional[str] - The iso code of the country.
+    :param yearid: Optional[int] - The year of the data.
     :param db: The database session.
-    :return: The data matching the iso code.
+    :return: Emissions data based on the provided criteria.
     """
-    return country_service.get_country_data_by_isocode(db, countryIsocode)
-
-
-@router.get("/country/c_isocode/{countryIsocode}/year/{yearid}")
-async def get_country_by_isocode_and_year(countryIsocode: str, yearid: int, db=db_dependency):
-    """
-    Retrieve country data by country iso code and year.
-
-    Name: '/country/c_isocode/{countryIsocode}/year/{yearid}'
-
-    Access: GET /country/c_isocode/{countryIsocode}/year/{yearid}
-        - countryIsocode: String - The iso code of the country.
-        - yearid: int - The year of the data.
-
-    :param countryIsocode: The iso code of the country.
-    :param yearid: The year of the data.
-    :param db: The database session.
-    :return: The data matching the iso code and year.
-    """
-    return country_service.get_country_data_by_isocode_and_year(db, countryIsocode, yearid)
-
-
-@router.put("/country/c_isocode/{countryIsocode}")
-async def update_country_by_isocode(countryIsocode: str, countrydt: CountryDataRequest, db=db_dependency):
-    """
-    Update a country by iso code.
-
-    Name: '/country/c_isocode/{countryIsocode}'
-
-    Access: PUT /country/c_isocode/{countryIsocode}
-        - countryIsocode: String - The iso code of the country to be updated.
-
-    :param countryIsocode: The iso code of the country to be updated.
-
-    :param countrydt: A country data request object containing the data to be updated Allows for
-    validation.
-
-    :param db: The database session.
-
-    :return: Nothing
-    """
-    return country_service.update_country_data_by_isocode(db, countryIsocode, countrydt)
-
-
-@router.delete("/country/c_isocode/{countryIsocode}/{yearid}")
-async def delete_country_by_isocode_and_year(countryIsocode: str, yearid: int, db=db_dependency):
-    """
-    Delete a country by iso code and year.
-
-    Name: '/country/c_isocode/{countryIsocode}/{yearid}'
-
-    Access: DELETE /country/c_isocode/{countryIsocode}/{yearid}
-        - countryIsocode: String - The iso code of the country to be deleted.
-        - yearid: int - The year of the data to be deleted.
-
-    :param countryIsocode: The iso code of the country to be deleted.
-
-    :param yearid: The year of the data to be deleted.
-
-    :param db: The database session.
-
-    :return: Nothing
-    """
-    return country_service.delete_country_data_by_isocode_and_year(db, countryIsocode, yearid)
-
-
-@router.get("/country/c_name/{countryName}/emissions")
-async def get_country_emissions_by_name(countryName: str, db=db_dependency):
-    """
-    Retrieve country emissions by country name.
-
-    Name: '/country/c_name/{countryName}/emissions'
-
-    Access: GET /country/c_name/{countryName}/emissions
-        - countryName: String - The name of the country.
-
-    :param countryName: The name of the country.
-
-    :param db: The database session.
-
-    :return: The emissions for that country.
-    """
-    return country_service.get_country_emissions_by_name(db, countryName)
-
-
-@router.get("/country/c_name/{countryName}/emissions/after/{yearid}")
-async def get_country_emissions_by_name_after_year(countryName: str, yearid: int, db=db_dependency):
-    """
-    Retrieve country emissions by country name and year.
-
-    Name: '/country/c_name/{countryName}/emissions/after/{yearid}'
-
-    Access: GET /country/c_name/{countryName}/emissions/after/{yearid}
-        - countryName: String - The name of the country.
-        - yearid: int - The beggining year of the data.
-
-    :param countryName: The name of the country.
-
-    :param yearid: The beggining year of the data.
-
-    :param db: The database session.
-
-    :return: Nothing
-    """
-    return country_service.get_country_emissions_by_name_after_year(db, countryName, yearid)
-
-
-@router.get("/country/c_isocode/{countryIsocode}/emissions")
-async def get_country_emissions_by_isocode(countryIsocode: str, db=db_dependency):
-    """
-    Retrieve country emissions by country iso code.
-
-    Name: '/country/c_isocode/{countryIsocode}/emissions'
-
-    Access: GET /country/c_isocode/{countryIsocode}/emissions
-        - countryIsocode: String - The iso code of the country.
-
-    :param countryIsocode: The iso code of the country.
-
-    :param db: The database session.
-
-    :return: Nothing
-    """
-    return country_service.get_country_emissions_by_isocode(db, countryIsocode)
-
-
-@router.get("/country/c_isocode/{countryIsocode}/emissions/after/{yearid}")
-async def get_country_emissions_by_isocode_after_year(countryIsocode: str, yearid: int, db=db_dependency):
-    """
-    Retrieve country emissions by country iso code and year.
-
-    Name: '/country/c_isocode/{countryIsocode}/emissions/after/{yearid}'
-
-    Access: GET /country/c_isocode/{countryIsocode}/emissions/after/{yearid}
-        - countryIsocode: String - The iso code of the country.
-        - yearid: int - The beggining year of the data.
-
-    :param countryIsocode: The iso code of the country.
-
-    :param yearid: The beggining year of the data.
-
-    :param db: The database session.
-
-    :return: The emissions for that country.
-    """
-    return country_service.get_country_emissions_by_isocode_after_year(db, countryIsocode, yearid)
-
-
-@router.get("/country/energyPerCapitaAndGDP/{yearid}")
-async def get_energy_per_capita_by_year(yearid: int, db=db_dependency):
-    """
-    Retrieve energy per capita by year.
-
-    Name: '/country/energyPerCapita/{yearid}'
-
-    Access: GET /country/energyPerCapita/{yearid}
-        - yearid: int - The year of the data.
-
-    :param yearid: The year of the data.
-
-    :param db: The database session.
-
-    :return: The energy per capita for that year.
-    """
-    return country_service.get_country_energy_by_year(db, yearid)
-
-
-@router.get("/countries/climateContribution/{year}")
-async def get_climate_contribution_by_year(year: int, n: int, order: str = Query('asc', regex='^(asc|desc)$'),
-                                           db=db_dependency):
-    """
-    Retrieve climate contribution by year.
-
-    Name: '/countries/climateContribution/{year}'
-
-    Access: GET /countries/climateContribution/{year}
-        - year: int - The year of the data.
-
-    :param year: The year of the data.
-
-    :param n: The number of countries to return.
-
-    :param order: The order of the data.
-
-    :param db: The database session.
-
-    :return: The climate contribution for that year.
-    """
-    return await country_service.get_climate_contribution_by_year(db, year, n, order)
-
-
-# TODO: double check this as I dont back my regex
-@router.get("/countries/climateContribution/previousYears")
-async def get_climate_contribution_previous_years(m: int, n: int,
-                                                  order: str = Query('asc', regex='^(asc|desc)$'), db=db_dependency):
-    """
-    Retrieve climate contribution by year.
-
-    Name: '/countries/climateContribution/previousYears'
-
-    Access: GET /countries/climateContribution/previousYears
-        - m: int - The year of the data.
-        - n: int - The number of countries to return.
-        - order: String - The order of the data.
-
-    :param m: The year of the data.
-
-    :param n: The number of countries to return.
-
-    :param order: The order of the data.
-
-    :param db: The database session.
-
-    :return: The climate contribution for that year.
-    """
-    return await country_service.get_climate_contribution_previous_years(db, m, n, order)
+    if countryName and yearid:
+        return country_service.get_country_emissions_by_name_after_year(db, countryName, yearid)
+    elif countryIsocode and yearid:
+        return country_service.get_country_emissions_by_isocode_after_year(db, countryIsocode, yearid)
+    elif countryName:
+        return country_service.get_country_emissions_by_name
--- services/country_service.py
+++ services/country_service.py
@@ -4,12 +4,48 @@ from sqlalchemy.orm import load_only
 from models import CountryData
 
 
-def get_country_data_by_name_and_year(db, country_name, yearid):
-    #first100
-    result = db.query(CountryData).filter(CountryData.country == country_name, CountryData.year == yearid).all()
-    if not result:
-        raise HTTPException(status_code=404, detail='Item not found')
+def get_country_data(db, countryName, iso, yearid, timeFrame):
+    result = None
+    if countryName:
+        if timeFrame == 'before':
+            result = db.query(CountryData).filter(CountryData.country == countryName,
+                                                  CountryData.year < yearid).all()
+        elif timeFrame == 'after':
+            result = db.query(CountryData).filter(CountryData.country == countryName,
+                                                  CountryData.year > yearid).all()
+        elif timeFrame == 'equal':
+            result = db.query(CountryData).filter(CountryData.country == countryName,
+                                                  CountryData.year == yearid).all()
+        elif yearid is None and timeFrame is None:
+            result = db.query(CountryData).filter(CountryData.country == countryName,
+                                                  CountryData.year == yearid).all()
+    elif iso:
+        if timeFrame == 'before':
+            result = db.query(CountryData).filter(CountryData.iso_code == iso,
+                                                  CountryData.year < yearid).all()
+        elif timeFrame == 'after':
+            result = db.query(CountryData).filter(CountryData.iso_code == iso,
+                                                  CountryData.year > yearid).all()
+        elif timeFrame == 'equal':
+            result = db.query(CountryData).filter(CountryData.iso_code == iso,
+                                                  CountryData.year == yearid).all()
+        elif yearid is None and timeFrame is None:
+            result = db.query(CountryData).filter(CountryData.iso_code == iso,
+                                                  CountryData.year == yearid).all()
     return result
+
+
+
+def delete_country_data_by_name_and_year_and_timeFrame(db, countryName, yearid, timeFrame):
+    if timeFrame == 'before':
+        result = db.query(CountryData).filter(CountryData.country == countryName,
+                                              CountryData.year < yearid).all()
+    elif timeFrame == 'after':
+        result = db.query(CountryData).filter(CountryData.country == countryName,
+                                              CountryData.year > yearid).all()
+    elif timeFrame == 'equal':
+        result = db.query(CountryData).filter(CountryData.country == countryName,
+                                              CountryData.year == yearid).all()
 
 
 def create_country_data(db, country_data_request):
@@ -31,32 +67,8 @@ def update_country_data_by_name(db, country_name, updated_data):
     return existing_data
 
 
-def delete_country_data_by_name_and_year(db, country_name, yearid):
-    data_to_delete = (db.query(CountryData).
-                      filter(CountryData.country == country_name, CountryData.year == yearid).first())
-    if not data_to_delete:
-        raise HTTPException(status_code=404, detail='Item not found')
-    db.delete(data_to_delete)
-    db.commit()
-    return {"detail": "Deleted successfully"}
-
-
 def get_all_data(db):
     result = db.query(CountryData).first()
-    return result
-
-
-def get_country_data_by_isocode(db, countryIsocode):
-    result = db.query(CountryData).filter(CountryData.iso_code == countryIsocode).all()
-    if not result:
-        raise HTTPException(status_code=404, detail='Item not found')
-    return result
-
-
-def get_country_data_by_isocode_and_year(db, countryIsocode, yearid):
-    result = db.query(CountryData).filter(CountryData.iso_code == countryIsocode, CountryData.year == yearid).all()
-    if not result:
-        raise HTTPException(status_code=404, detail='Item not found')
     return result
 
 
@@ -90,7 +102,7 @@ def get_country_emissions_by_name(db, countryName):
             CountryData.methane,
             CountryData.nitrous_oxide,
             CountryData.total_ghg)
-        ).all()
+    ).all()
     if not result:
         raise HTTPException(status_code=404, detail='Item not found')
     return result
@@ -122,7 +134,8 @@ def get_country_emissions_by_name_after_year(db, countryName, yearid):
         raise HTTPException(status_code=404, detail='Item not found')
     return result
 
-#TODO Fix filtering on two things, this gives internal server error.
+
+# TODO Fix filtering on two things, this gives internal server error.
 def get_country_emissions_by_isocode_after_year(db, countryIsocode, yearid):
     result = db.query(CountryData.country,
                       CountryData.co2,
@@ -133,6 +146,7 @@ def get_country_emissions_by_isocode_after_year(db, countryIsocode, yearid):
     if not result:
         raise HTTPException(status_code=404, detail='Item not found')
     return result
+
 
 def get_country_energy_by_year(db, yearid):
     result = db.query(CountryData).filter(CountryData.year == yearid).options(
@@ -148,9 +162,10 @@ def get_country_energy_by_year(db, yearid):
         raise HTTPException(status_code=404, detail='Item not found')
     return result
 
+
 # TODO: Fix the ordering
-#Hi Max, Arnaud here. I just found that you can do .order_by(asc|desc( ' what you want to filter by))
-#I presume this will be helpful for this part. I don't think you need to do the regex.
+# Hi Max, Arnaud here. I just found that you can do .order_by(asc|desc( ' what you want to filter by))
+# I presume this will be helpful for this part. I don't think you need to do the regex.
 def get_climate_contribution_by_year(db, year, n, order):
     if order == 'asc':
         result = db.query(CountryData.country,
@@ -169,6 +184,7 @@ def get_climate_contribution_by_year(db, year, n, order):
     if not result:
         raise HTTPException(status_code=404, detail='Item not found')
     return result
+
 
 # TODO: Fix the ordering
 def get_climate_contribution_previous_years(db, m, n, order):
@@ -189,3 +205,4 @@ def get_climate_contribution_previous_years(db, m, n, order):
     if not result:
         raise HTTPException(status_code=404, detail='Item not found')
     return result
+
